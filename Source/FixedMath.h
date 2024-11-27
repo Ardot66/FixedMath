a7adf3aced968e4f80e00a913a20a9ff62700127
Gabriel Clay <gabrielwclay@gmail.com>
2024-11-27 19:48:13 +1100

    Set up basic Fixed type and High-Low functions

--- Source/FixedMath.h
+++ Source/FixedMath.h
@@ -1,6 +1,30 @@
 #ifndef __FIXED_MATH__
 #define __FIXED_MATH__
 
+#include <limits.h>
+#include <stdint.h>
 
+#define FIXED_T int64_t
+#define FIXED_PARTITION 16
+
+#define FIXED_SIZE (sizeof(FIXED_T) * CHAR_BIT)
+
+#define FIXED(High, Low) ((High << 16) + Low)
+
+typedef FIXED_T Fixed;
+
+static inline FIXED_T FixedHigh(Fixed fixed)
+{
+    return fixed >> FIXED_PARTITION;
+}
+
+static inline FIXED_T FixedLow(Fixed fixed)
+{
+    return (fixed << (FIXED_SIZE - FIXED_PARTITION)) >> (FIXED_SIZE - FIXED_PARTITION);
+}
+
+#undef FIXED_T
+#undef FIXED_PARTITION
+#undef FIXED_SIZE
 
 #endif
--- Tests/Tests.c
+++ Tests/Tests.c
@@ -4,5 +4,10 @@
 
 int main(int argCount, char **argValues)
 {
+    Fixed fixed = FIXED(100, 156);
+    
+    TEST(FixedHigh(fixed), ==, 100, llu)
+    TEST(FixedLow(fixed), ==, 156, llu)
+
     TestsEnd();
 }
